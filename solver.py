a6bc31e7ceb46e49913271588a713ffe21c670d1
Aldo <acasareto@gmail.com>
2023-06-24 08:36:51 +0200

    first commit

--- solver.py
+++ solver.py
@@ -1,13 +1,41 @@
 import requests
 
-def wordle_solver(letters, exclude_letters="", word_length=5):
+
+def wordle_solver():
+    exclude_letters = ""
+    while True:
+        print(
+            "Enter 'q' to quit, 'h' for help, or the known letters of the secret word:"
+        )
+        letters = input("> ")
+        if letters == "q":
+            break
+        elif letters == "h":
+            print("Help:")
+            print(
+                "- Enter the known letters of the secret word, replacing any unknown letters with a '*'."
+            )
+            print(
+                f"- To exclude certain letters from the search, enter them after the prompt (current excluded letters: {exclude_letters})."
+            )
+            print("- Enter 'q' to quit the program.")
+        else:
+            exclude_letters = input("Enter any letters to exclude: ")
+            solutions = get_solutions(letters, exclude_letters)
+            print(f"Possible solutions: {solutions}")
+
+
+def get_solutions(letters, exclude_letters="", word_length=5):
     url = f"https://api.datamuse.com/words?sp={letters}*&max=1000"
     response = requests.get(url)
     words = response.json()
-    solutions = [word["word"] for word in words if len(word["word"]) == word_length and all(letter not in word["word"] for letter in exclude_letters)]
+    solutions = [
+        word["word"]
+        for word in words
+        if len(word["word"]) == word_length
+        and all(letter not in word["word"] for letter in exclude_letters)
+    ]
     return solutions
 
-letters = input("Enter the known letters: ")
-exclude_letters = input("Enter any letters to exclude: ")
-solutions = wordle_solver(letters, exclude_letters)
-print(f"Possible solutions: {solutions}")
+
+wordle_solver()
